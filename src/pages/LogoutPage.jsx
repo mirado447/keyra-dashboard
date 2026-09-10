5b8b07b7a0a12cd1cd606d01c7d21b174f542bea
Mirado <hei.mirado@gmail.com>
2026-09-10 20:39:08 +0300

    refactor: add navigate to login after logout

--- src/pages/LogoutPage.jsx
+++ src/pages/LogoutPage.jsx
@@ -1,9 +1,17 @@
+import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/useAuth";
 
+//Remplacer cette page par un simple bouton onClick={handleLogout} dans le header ou navbar, sans route associée.
 function LogoutPage() {
     const { logout } = useAuth();
+    const navigate = useNavigate();
 
-    return <button onClick={logout}>Se déconnecter</button>
+    function handleLogout() {
+        logout();
+        navigate("/login");
+    }
+
+    return <button onClick={handleLogout}>Se déconnecter</button>
 }
 
 export default LogoutPage;
